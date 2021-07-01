fd182109418a2cffefd80383f44cce6d19882cb3
helishou <479525390@qq.com>
2021-07-02 00:45:48 +0800

    111

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -17,63 +17,53 @@ const List = (props) => {
   ];
   return (
     <BlogList>
-      {list.map((item, index) => {
-        return (
-          <div className={Class[index % Class.length]} key={index}>
-            <div className="post-thumb">
-              <Link to={"/article/" + item.id}>
-                <img src={item.thumbnail} alt="" />
-              </Link>
-            </div>
-            <div className="post-content-wrap">
-              <div className="post-content">
-                <div className="post-date">
-                  <i className="iconfont icon-time" />
-                  发布于{getTime(item.createTime)}
-                </div>
-                <Link to={"/article/" + item.id} className="post-title">
-                  <h3>{item.title}</h3>
-                </Link>
-              </div>
-              <div className="post-content-wrap">
-                <div className="post-content">
-                  <div className="post-date">
-                    <i className="iconfont icon-time" />
-                    发布于 {getTime(item.createTime)}
-                  </div>
-                  <Link to={"/article/" + item.id} className="post-title">
-                    <h3>{item.title}</h3>
-                  </Link>
-                  <div className="post-meta">
-                    <span>
-                      <i className="iconfont icon-file" />
-                      {item.categoryName[0]}
-                    </span>
-                    <span className="comments-number">
-                      <i className="iconfont icon-mark" />
-                      {item.comments} 评论
-                    </span>
-                    {item.categoryName && (
-                      <span>
-                        <i className="iconfont icon-file" />
-                        {item.categoryName}
-                      </span>
-                    )}
-                  </div>
-                  <div className="float-content">
-                    <p>{item.summary}</p>
-                    <div className="post-bottom">
-                      <Link to={"/article/" + item.id}>
-                        <i className="iconfont icon-caidan" />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    {list.map((item, index) => {
+      return (
+        <div className={Class[index % Class.length]} key={index}>
+          <div className="post-thumb">
+            <Link to={"/article/" + item.id}>
+              <img src={item.thumbnail} alt="" />
+            </Link>
           </div>
-        );
-      })}
-    </BlogList>
+          <div className="post-content-wrap">
+            <div className="post-content">
+              <div className="post-date">
+                <i className="iconfont icon-time" />
+                发布于 {getTime(item.createTime)}
+              </div>
+              <Link to={"/article/" + item.id} className="post-title">
+                <h3>{item.title}</h3>
+              </Link>
+              <div className="post-meta">
+                <span>
+                  <i className="iconfont icon-attention" />
+                  {item.views} 热度
+                </span>
+                <span className="comments-number">
+                  <i className="iconfont icon-mark" />
+                  {item.comments} 评论
+                </span>
+                {item.categoryName && (
+                  <span>
+                    <i className="iconfont icon-file" />
+                    {item.categoryName}
+                  </span>
+                )}
+              </div>
+              <div className="float-content">
+                <p>{item.summary}</p>
+                <div className="post-bottom">
+                  <Link to={"/article/" + item.id}>
+                    <i className="iconfont icon-caidan" />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    })}
+  </BlogList>
   );
 };
 
