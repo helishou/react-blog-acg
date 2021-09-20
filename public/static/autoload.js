502c4e348250a49dd4e63d7b225e993893b48e79
helishou <479525390@qq.com>
2021-09-20 13:52:47 +0800

    111

--- public/static/autoload.js
+++ public/static/autoload.js
@@ -1,9 +1,9 @@
 try {
     if (!isMobileBrowser()) {
-        $("<link>").attr({href: "/assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+        $("<link>").attr({href: "/static/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
         $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"/><div class="waifu-tool"><span class="fui-home"/> <span class="fui-chat"/> <span class="fui-eye"/> <span class="fui-user"/> <span class="fui-photo"/> <span class="fui-info-circle"/> <span class="fui-cross"/></div></div>');
-        $.ajax({url: '/assets/waifu-tips.min.js?v=1.4.2', dataType: "script", cache: true, async: false});
-        $.ajax({url: '/assets/live2d.min.js?v=1.0.5', dataType: "script", cache: true, async: false});
+        $.ajax({url: '/static/waifu-tips.min.js?v=1.4.2', dataType: "script", cache: true, async: false});
+        $.ajax({url: '/static/live2d.min.js?v=1.0.5', dataType: "script", cache: true, async: false});
         /* 可直接修改部分参数 */
         live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
         live2d_settings['modelId'] = 6;                  // 默认模型 ID
