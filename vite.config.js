46f2c5084c8625dbc12d905ce13474dba1e52d28
wangxinyang11 <wangxinyang11@meituan.com>
2022-06-23 12:50:28 +0800

    feat: 增加打包配置

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+    Components(
+        {
+            resolvers: [AntDesignVueResolver()]
+        }
+    )],
     server: {
         proxy: {
             '/api': {
