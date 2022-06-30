324aa34b12683c230f07b72f4035d1000611ba1a
wangxinyang11 <wangxinyang11@meituan.com>
2022-06-30 16:24:32 +0800

    style:  把cur文件传到云端

--- src/lib/constans/vite.js
+++ src/lib/constans/vite.js
@@ -1,9 +1,15 @@
-// export const MOUSEPOINTCUR= new URL('@/assets/images/ayuda.cur', import.meta.url).href;
-// export const TEXTOCUR = new URL('@/assets/images/texto.cur', import.meta.url).href
-// export const HORSERACELAMP = new URL('@/assets/images/HorseRaceLamp.gif', import.meta.url).href
+// const MOUSEPOINTCUR= new URL('@/assets/images/ayuda.cur', import.meta.url).href;
+// const TEXTOCUR = new URL('@/assets/images/texto.cur', import.meta.url).href
 
-import MOUSEPOINTCUR from "@/assets/images/ayuda.cur";
-import TEXTOCUR from "@/assets/images/texto.cur";
+// import MOUSEPOINTCUR from "@/assets/images/ayuda.cur";
+// import TEXTOCUR from "@/assets/images/texto.cur";
+// import HORSERACELAMP from "@/assets/images/HorseRaceLamp.gif";
 import HORSERACELAMP from "@/assets/images/HorseRaceLamp.gif";
+const MOUSEPOINTCUR =
+  "https://wxy-picture.oss-cn-hangzhou.aliyuncs.com/ayuda.cur";
+const TEXTOCUR = "https://wxy-picture.oss-cn-hangzhou.aliyuncs.com/texto.cur";
+
 
 export { MOUSEPOINTCUR, TEXTOCUR, HORSERACELAMP };
+
+// const HORSERACELAMP = new URL('@/assets/images/HorseRaceLamp.gif', import.meta.url).href
