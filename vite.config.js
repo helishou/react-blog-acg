3c831471afa52cba7471fac4a96ea998aa73854a
wangxinyang11 <wangxinyang11@meituan.com>
2022-06-30 19:21:21 +0800

    fix: 去除无用依赖

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 import path from "path";
 import commonjs from 'vite-plugin-commonjs'
