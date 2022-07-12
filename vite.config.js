8fcd17276725f26b017ae9fccb1d8aba9c5806a7
helishou <479525390@qq.com>
2022-07-12 16:16:47 +0800

    feat: 去除按需导入

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
-import path from "path";
-import commonjs from 'vite-plugin-commonjs'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import path from 'path';
+import commonjs from 'vite-plugin-commonjs';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    commonjs,
-    react(),
-  ],
-  optimizeDeps:{
+  plugins: [commonjs, react()],
+  optimizeDeps: {
     // exclude:['lodash-es']
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://47.98.154.142/",
+      '/api': {
+        target: 'http://47.98.154.142/',
         changeOrigin: true,
       },
     },
@@ -26,13 +23,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    rollupOptions: {
-      plugins: [dynamicImportVars()],
-    },
+    rollupOptions: {},
     // sourcemap: true,
   },
 });
